746f468ee0f47f96454ecd3b24dfea4adc398e7c
Sai Lokesh <92055028+Nightknife@users.noreply.github.com>
2022-11-30 23:33:00 +0530

    Update PhysicsFX.java

--- src/physicsfx/PhysicsFX.java
+++ src/physicsfx/PhysicsFX.java
@@ -5,10 +5,7 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-/**
- *
- * @author Jodi
- */
+
 public class PhysicsFX extends Application {
     
     @Override
